7121c5cb7f5d41d29ff97951fc6f40eec14885de
guyezi <7312140@qq.com>
2019-11-18 03:47:30 +0800

    版本信息

---
+++ precache-manifest.144931cd4bbaa09b4791678bdeda51a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "231b475e6c88d4c5a316",
+    "url": "/shared-library-ui/static/css/main.7e690c6d.chunk.css"
+  },
+  {
+    "revision": "231b475e6c88d4c5a316",
+    "url": "/shared-library-ui/static/js/main.231b475e.chunk.js"
+  },
+  {
+    "revision": "e2aaf6950cd2fbea273f",
+    "url": "/shared-library-ui/static/css/1.e26214f6.chunk.css"
+  },
+  {
+    "revision": "e2aaf6950cd2fbea273f",
+    "url": "/shared-library-ui/static/js/1.e2aaf695.chunk.js"
+  },
+  {
+    "revision": "78c3d17d800ab2c26f17",
+    "url": "/shared-library-ui/static/js/runtime~main.78c3d17d.js"
+  },
+  {
+    "revision": "5caf7bfcd6710104a42154534fad07af",
+    "url": "/shared-library-ui/static/media/pdf_s.5caf7bfc.png"
+  },
+  {
+    "revision": "1e078ab284ddf5ee8b213aa0dc92167a",
+    "url": "/shared-library-ui/static/media/word_s.1e078ab2.png"
+  },
+  {
+    "revision": "6167687fcbd63fe9dca50ffd44a2bff3",
+    "url": "/shared-library-ui/static/media/word_l.6167687f.png"
+  },
+  {
+    "revision": "ae4e37a88fb9d386101022770886b808",
+    "url": "/shared-library-ui/static/media/pdf_l.ae4e37a8.png"
+  },
+  {
+    "revision": "edd2a89cdf2ee0518be0a87c3b23f101",
+    "url": "/shared-library-ui/static/media/banner1.edd2a89c.jpg"
+  },
+  {
+    "revision": "6f240434afe2745189d30c93017140b2",
+    "url": "/shared-library-ui/static/media/banner2.6f240434.jpg"
+  },
+  {
+    "revision": "0d4920cece20634af0039a784c64bbda",
+    "url": "/shared-library-ui/static/media/banner3.0d4920ce.jpg"
+  },
+  {
+    "revision": "bf0cfeac0b7babec318cc0887bb396fa",
+    "url": "/shared-library-ui/static/media/cover-news.bf0cfeac.png"
+  },
+  {
+    "revision": "02f4269a472db8747d9f5a492758db45",
+    "url": "/shared-library-ui/static/media/logo.02f4269a.png"
+  },
+  {
+    "revision": "4ced7bab9d66a0e6a9b08ac4983fe2a7",
+    "url": "/shared-library-ui/index.html"
+  }
+];
